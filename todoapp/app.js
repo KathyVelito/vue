ffa82f0299de9ba97370996de07f9103881d576e
KathyVelito <85207829+KathyVelito@users.noreply.github.com>
2022-02-13 15:32:56 -0500

    first commit

---
+++ todoapp/app.js
@@ -0,0 +1,23 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Tareas por hacer con Vue',
+        tareas: [],
+        nuevaTarea: ''
+    },
+    methods: {
+        agregarTarea: function() {
+            this.tareas.push({
+                nombre: this.nuevaTarea,
+                estado: false
+            });
+            this.nuevaTarea = '';
+        },
+        editarTarea: function (index) {
+            this.tareas[index].estado = true;
+        },
+        eliminarTarea: function (index) {
+            this.tareas.splice(index, 1);
+        }
+    }
+});
